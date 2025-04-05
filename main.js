300f40e1d93eb29555fedab1b607f7c6fe23a9f5
さとしん <131766015+satoshin-des@users.noreply.github.com>
2025-04-05 11:25:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+let jsonData;
 
+fetch('data.json')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    jsonData = data;
+  });
+
+/**
+* 用語を日本語に翻訳する関数
+*/
 function myReplace(node, str) {
   if (node.nodeType ===  Node.TEXT_NODE) {
-    node.textContent = node.textContent.replace(new RegExp(str, "g"), json hoge);
+    node.textContent = node.textContent.replace(new RegExp(str, "g"), json[str]);
   } else {
     node.childNodes.forEach(myReplace);
   }
