2b6970fe28edfc3c8a6da5feba88d100431b68bc
さとしん <131766015+satoshin-des@users.noreply.github.com>
2025-04-05 18:20:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,13 @@
 function myReplace(node) {
   if (node.nodeType === Node.TEXT_NODE) {
     for (const str in DATA) {
-      const regex = new RegExp(str, "g");
-      node.textContent = node.textContent.replace(regex, DATA[str]);
-      console.log(regex);
+      node.textContent = node.textContent.replace(RegExp(str, "g"), DATA[str]);
     }
   } else {
     node.childNodes.forEach(myReplace);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("here");
+window.onload = function () {
   myReplace(document.body);
-});
+}
