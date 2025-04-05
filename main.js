e46d0499fc13a23673c993834bc1c164102a07d3
さとしん <131766015+satoshin-des@users.noreply.github.com>
2025-04-05 17:53:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 /**
 * 用語を日本語に翻訳する関数
 */
-function myReplace(node) {
+function myReplace(node, str) {
   if (node.nodeType ===  Node.TEXT_NODE) {
-    for (const str in DATA) {
-      const regex = new RegExp(str, "g");
-      node.textContent = node.textContent.replace(regex, DATA[str]);
-    }
+    const regex = new RegExp(str, "g");
+    node.textContent = node.textContent.replace(regex, DATA[str]);
   } else {
     node.childNodes.forEach(myReplace);
   }
 }
 
 window.onload = function () {
-  myReplace(document.body);
+  for (const str in DATA) {
+    myReplace(document.body, str);
+  }
 }
