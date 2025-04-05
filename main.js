388a60f8ad0a4f2e5b452d8ab0f35b51be37b531
さとしん <131766015+satoshin-des@users.noreply.github.com>
2025-04-05 20:00:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,12 +5,24 @@ function myReplace(node) {
   if (node.nodeType === Node.TEXT_NODE) {
     for (const str in DATA) {
       node.textContent = node.textContent.replace(RegExp(str, "g"), DATA[str]);
+      console.log(str);
     }
   } else {
     node.childNodes.forEach(myReplace);
   }
 }
 
-window.onload = function () {
-  myReplace(document.body);
-}
+myReplace(document.body);
+
+const obs = new MutationObserver((mutations) => {
+  for(const mutation of mutations) {
+    for(const node of mutation.addedNodes) {
+      myReplace(node);
+    }
+  }
+});
+
+obs.observe(document.body, {
+  childList: true,
+  subtree: true
+});
