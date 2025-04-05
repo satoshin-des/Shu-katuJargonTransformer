5cc078170747f43c82c187486e91724c7db883d5
さとしん <131766015+satoshin-des@users.noreply.github.com>
2025-04-05 11:16:12 +0900

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,19 @@
+{
+    "name": "Chromingorou",
+    "version": "0.0.1",
+    "manifest_version": 3,
+    "author": "satoshin",
+    "icons": {
+        "48": "icon/icon.png"
+    },
+    "description": "就活で出くわすIT用語などを日本語に翻訳してくれる拡張機能．",
+    "content_scripts": [
+        {
+            "matches": [
+                "<all_urls>"
+            ],
+            "js": ["main.js"]
+        }
+    ],
+    "homepage_url": "https://github.com/satoshin-des/Shu-katuJargonTransformer"
+}
